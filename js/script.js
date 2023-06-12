211cbf93077ee6197d850429d572dac6ec4640ed
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2023-06-12 18:48:38 -0300

    Consertando imagem de background

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const polyrhythmDOM = new PolyrhythmDOM();
 
-const background = polyrhythmDOM.buildBackgroundImage("../assets/backgrounds/background.gif");
+const background = polyrhythmDOM.buildBackgroundImage("assets/backgrounds/background.gif");
 const canvas = polyrhythmDOM.buildCanvas();
 
 const polyrhythm = new Polyrhythm(canvas);
